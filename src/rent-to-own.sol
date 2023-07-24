94c94d97e8739da124fead0e01b77483ea9fcff6
Lars van Vianen <lars@vianen.com>
2023-07-24 12:25:23 +0200

    Update rent-to-own.sol

--- src/rent-to-own.sol
+++ src/rent-to-own.sol
@@ -1,6 +1,11 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache-2.0
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.19;
+
+
+// ============================================================================
+// Contracts
+// ============================================================================
 
 contract RentToOwn {
 
